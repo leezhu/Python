828333a8f8adf475485e4b5f0562ddfe06cccee9
leezhu <leezhu@hnu.edu.cn>
2016-09-02 16:51:00 +0800

    zhulie

---
+++ thread.py
@@ -0,0 +1,18 @@
+#encoding=utf-8
+import threading
+import time
+
+class MyThread(threading.Thread):
+    def run(self):
+        for i in range(3):
+            time.sleep(3)
+            msg = "I`m"+self.name +'@'+str(i)
+            print msg
+
+def test():
+    for i in range(5):
+        t = MyThread()
+        t.start()
+
+if __name__ == '__main__'
+    test()
